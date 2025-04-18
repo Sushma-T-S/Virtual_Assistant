3cd6cff3987b961ff6bc35fb25998bbcaa8c5cf9
Sushma-T-S <sush2112004@gmail.com>
2025-04-18 11:09:05 +0530

    day 3\n

--- www/main.js
+++ www/main.js
@@ -1,12 +1,41 @@
+
 $(document).ready(function () {
-    $('.text').textillate({
-        loop: true,
-        sync:true,
-        in:{
-            effect:"bounceIn",
-        },
-        out:{
-            effect:"bounceOut",
-        }
-    })
+
+  $('.text').textillate({
+      loop: true,
+      sync: true,
+      in: {
+          effect: "bounceIn",
+      },
+      out: {
+          effect: "bounceOut",
+      },
+
+  });
+
+
+//siri message animation
+$('.siri-message').textillate({
+  loop: true,
+  sync: true,
+  in: {
+      effect: "fadeInUp",
+      sync:true,
+  },
+  out: {
+    effect: "fadeOutUp",
+    sync:true,
+  },
+
 });
+});
+  // Siri configuration
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 640,
+    height: 200,
+    style:"ios9",
+    amplitude:"1",
+    speed:"0.30",
+    autostart:true
+  });
